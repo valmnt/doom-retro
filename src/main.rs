5814934982c86666b3345c982cf70b8f4edfd096
Valentin Mont <valentinmont8@gmail.com>
2025-11-17 20:29:04 +0100

    refactor: move game logic in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 mod engine;
-mod game;
-mod map;
-mod player;
 
-use game::Game;
 use macroquad::prelude::*;
 use std::f32::consts::PI;
 
-use crate::{
-    engine::Engine,
-    map::{MAP, MAP_HEIGHT, MAP_WIDTH, TILE_SIZE},
-};
+use crate::engine::Engine;
 
 #[macroquad::main(conf)]
 async fn main() {
@@ -44,3 +37,72 @@ fn conf() -> Conf {
         ..Default::default()
     }
 }
+
+struct Game {
+    pub player: Player,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {
+            player: Player {
+                pos: vec2(2.0 * TILE_SIZE, 1.0 * TILE_SIZE),
+                angle: 0.0,
+            },
+        }
+    }
+}
+
+struct Player {
+    pub pos: Vec2,
+    pub angle: f32,
+}
+
+impl Player {
+    pub fn handle_key(&mut self, engine: &Engine, dt: f32) {
+        const MOVE_SPEED: f32 = 120.0;
+        const ROT_SPEED: f32 = 3.0;
+
+        let dir = vec2(self.angle.cos(), self.angle.sin());
+        let mut delta = Vec2::ZERO;
+
+        if is_key_down(KeyCode::Left) {
+            self.angle -= ROT_SPEED * dt;
+        }
+        if is_key_down(KeyCode::Right) {
+            self.angle += ROT_SPEED * dt;
+        }
+
+        if is_key_down(KeyCode::Up) {
+            delta += dir;
+        }
+        if is_key_down(KeyCode::Down) {
+            delta -= dir;
+        }
+
+        if delta.length_squared() > 0.0 {
+            delta = delta.normalize() * MOVE_SPEED * dt;
+            engine.try_move(&mut self.pos, delta);
+        }
+
+        engine.fiel_of_view(self.pos, dir);
+    }
+}
+
+#[rustfmt::skip]
+const MAP: [u8; MAP_WIDTH * MAP_HEIGHT] = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 1, 1, 0, 1,
+    1, 0, 1, 0, 1, 0, 1, 0, 0, 1,
+    1, 0, 1, 0, 1, 0, 1, 0, 1, 1,
+    1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+    1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1, 0, 1,
+    1, 0, 1, 1, 1, 1, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
+
+const MAP_WIDTH: usize = 10;
+const MAP_HEIGHT: usize = 10;
+const TILE_SIZE: f32 = 64.0;
